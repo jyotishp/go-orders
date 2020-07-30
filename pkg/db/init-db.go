6eabef4915242999e0329692c715862033fa70dd
swap-pakhare <swapchamps@gmail.com>
2020-07-30 19:33:59 +0530

    Added db init scripts

---
+++ pkg/db/init-db.go
@@ -0,0 +1,60 @@
+package db
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"log"
+)
+
+func printAwsError(err error) {
+	if aerr, ok := err.(awserr.Error); ok {
+		switch aerr.Code() {
+		case dynamodb.ErrCodeInternalServerError:
+			fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
+		case dynamodb.ErrCodeResourceInUseException:
+			fmt.Println(dynamodb.ErrCodeResourceInUseException, aerr.Error())
+
+		default:
+			fmt.Println(aerr.Error())
+		}
+	} else {
+		// Print the error, cast err to awserr.Error to get the Code and
+		// Message from an error.
+		fmt.Println(err.Error())
+	}
+}
+
+func createSession() *dynamodb.DynamoDB {
+
+	sess := session.Must(session.NewSession(&aws.Config{
+		Endpoint: aws.String("http://localhost:8000"),
+		Region: aws.String("Mumbai"),
+	}))
+
+	return dynamodb.New(sess)
+}
+
+func checkTable(tableName string) bool  {
+
+	svc := createSession()
+
+	input := &dynamodb.ListTablesInput{}
+
+	for {
+		result, err := svc.ListTables(input)
+		if err != nil {
+			printAwsError(err)
+			return false
+		}
+
+		for _, table := range result.TableNames {
+			if *table == tableName {
+				return true
+			}
+		}
+		return false
+	}
+}
